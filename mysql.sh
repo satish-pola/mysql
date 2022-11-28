e3a709489adfe9c060563b3f77535868119cd30a
satish-pola <49170242+satish-pola@users.noreply.github.com>
2022-11-27 23:12:40 -0800

    Add files via upload

---
+++ mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#####################################
+## Set up log location variables   ##
+## 1. Log home (MYSQL_LOG_HOME)    ##
+## 2. Log file name (MYSQL_GEN_LOG)##
+## 3. Email list (EMAIL_LIST)      ##
+#####################################
+
+MYSQL_LOG_HOME="/opt/homebrew/var/mysql" 
+MYSQL_GEN_LOG="Satishs-MBP-2.attlocal.net.err"
+EMAIL_LIST="email1@intuit.com email2@intuit.com email3@intuit.com"
+
+############## 1. GENERAL LOG OPTION ############################
+## Uncomment one of the below "data" if the general log is enabled.
+## To simplify the ask, I am just sorting the unauthorized access by name.
+## We can also add a date column and group by date and users and find who accessed the system by days, hours, etc...
+
+# With date column
+#data=`grep -E "Init\ DB.*Access denied for" $MYSQL_LOG_HOME/$MYSQL_GEN_LOG | awk -F ' ' '{"date -d\"" $1 "\" +%Y-%m-%d" | getline extractdate; print extractdate " " $9 " " $12}' | uniq -c | sort -nr`
+# Without date column 
+#data=`grep -E "Init\ DB.*Access denied for" $MYSQL_LOG_HOME/$MYSQL_GEN_LOG | awk -F ' ' '{print $9 " " $12}' | uniq -c | sort -nr`
+
+################# 2. ERROR LOG OPTION ###########################
+## Use the below if the "access denied" messages are pushed to the error log.
+## make sure to use log_error_verbosity=3 in Mysql > 5.7.2 version and log_warnings=2 in <5.7.2 
+data=`grep -E "Access denied for user.* to database" $MYSQL_LOG_HOME/$MYSQL_GEN_LOG | awk -F ' ' '{print $10 " " $13}' | uniq -c | sort -nr`
+
+## Here I am using Option:2 and parsing the output. 
+while read -r line
+do
+   violations=`echo "$line" | cut -d" " -f1`
+   user=`echo "$line" | cut -d" " -f2`
+   database=`echo "$line" | cut -d" " -f3`
+   if [[ "$violations" -ge 10 ]]
+   then
+     echo "User $user violated $violations times to access the database $database" | mail -s "Violation" "$EMAIL_LIST"
+   fi
+done <<< "$data"
---
+++ parse_audit.py
@@ -0,0 +1,38 @@
+import json
+import subprocess
+
+## Setup MySQL home and Audit log file variables. 
+mysql_log_home='/opt/homebrew/var/mysql'
+audit_logfile='audit.log'
+audit_log_loc = mysql_log_home+'/'+audit_logfile
+email_list = "dbe@intuit.com db-oncall@intuit.com"
+
+## Function to send an violation emails. 
+## It sends who violated the rule and how many times.
+ 
+def sendEmail(username, violations, email_list):
+  usercall = "echo user " + username + " violated " + str(violations) + " times. |  mail -s MySQL_Violation " + email_list
+  print usercall
+  subprocess.call(usercall, shell=True)
+
+## Function accepts audit log as input and retuns user violations. 
+def parseJson(logfile):
+  user_list=[]
+  with open('audit.log') as audit_log:
+    audit_list = ['NoAudit','Audit']
+    for each in audit_log:
+      audit_data = json.loads(each)
+      if (audit_data['audit_record']['name'] not in audit_list) and (audit_data['audit_record']['status'] == 1044):
+        if '@' in (audit_data['audit_record']['user']):
+          user_list.append(audit_data['audit_record']['user'].split('@')[0].strip().split('[')[0])
+        else:
+          user_list.append(audit_data['audit_record']['user'])
+    user_list={x:user_list.count(x) for x in user_list}
+  return user_list
+
+user_list = parseJson(audit_log_loc)
+
+if __name__ == "__main__":
+  for key,val in user_list.items():
+    if val >= 10:
+      sendEmail(key, val, email_list)
